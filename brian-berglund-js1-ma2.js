ac84b2067c19464646692fee5233a2ad720eafb4
BrianEP-hub <54816472+BrianEP-hub@users.noreply.github.com>
2020-02-16 10:24:59 +0100

    Create brian-berglund-js1-ma2.js

---
+++ brian-berglund-js1-ma2.js
@@ -0,0 +1,106 @@
+//Question 1
+
+const myFunctionExpression = function()  {
+        console.log("Brian Berglund");
+};
+
+console.log(myFunctionExpression);
+
+
+//Question 2
+
+const button = document.querySelector("input");
+
+function buttonClick(){
+        console.log("I was clicked!");
+}
+
+button.addEventListener("click", clicked);
+
+//Question 3
+
+const key = document.querySelector("#firstName");
+
+function keyEvent(event) {
+      console.log("value: ", event.target.value)
+}
+
+key.addEventListener("keyup", keyEvent);
+
+//Question 4
+
+const mOver = document.querySelector("button");
+
+function hovOver(){
+      mOver.classList.add("hover");
+      console.dir(mOver.classList);
+}
+mOver.addEventListener("mouseover", hovering);
+
+//Question 5
+
+const mOut = document.querySelector("[data-animal='dog']");
+
+function out(){
+      mOut.classList.remove("hover");
+      console.dir(mOut.classList);
+}
+mOut.addEventListener("mouseout", out);
+
+//Question 6
+const animal = document.querySelectorAll("li");
+
+for (let i = 0; i < animal.length; i++){
+        animal[i].addEventListener("mouseover", list);
+}
+
+function list(event){
+      console.log(event.target.dataset.animal);
+}
+
+//Question 7
+
+const animals = "cow";
+switch(animals){
+    case "cat":
+    console.log("Meow");
+    break;
+    case: "cow":
+    console.log("Moo");
+    break;
+    case: "bird":
+    console.log("Tweet");
+    break
+    default:
+      console.log("Harrumph")
+}
+
+//Question 8
+
+const sheep = ["Malcolm", "Anders", "Marie"];
+    sheep.forEach(function(sheep){
+        console.log(sheep);
+    });
+
+//Question 9
+
+function timeLog(){
+      console.log("Hello!");
+
+      if(counter === 6){
+          clearInterval(intervalId);
+      }
+      counter++
+}
+let counter = 0
+const intervalID = setInterval(timeLog, 500);
+
+
+//Question 10
+const container = document.querySelector(".container");
+
+function textUpdate(){
+      container.innerText = "Text updated!";
+}
+
+setTimeout(textUpdate, 2000);
